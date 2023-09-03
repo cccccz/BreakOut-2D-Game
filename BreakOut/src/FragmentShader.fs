e15c7a95fe7173d7eb8cf638794ec4d7e80ec803
Zheng Chen <chenzheng0430@gmail.com>
2023-09-03 04:26:24 -0400

    up down arrow key can change the mixing betw textures

--- BreakOut/src/FragmentShader.fs
+++ BreakOut/src/FragmentShader.fs
@@ -6,10 +6,11 @@ in vec2 TexCoord;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
+uniform float mixValue;
 
 
 void main()
 {
     
-    FragColor = mix(texture(texture1, TexCoord),texture(texture2,vec2(1-TexCoord.x,TexCoord.y)),0.8);
+    FragColor = mix(texture(texture1, TexCoord),texture(texture2,vec2(1-TexCoord.x,TexCoord.y)),mixValue);
 }
--- BreakOut/src/app.cpp
+++ BreakOut/src/app.cpp
@@ -13,6 +13,8 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+float mixValue = 0.2f;
+
 int main()
 {
     // glfw: initialize and configure
@@ -169,6 +171,7 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
+        ourShader.setFloat("mixValue", mixValue);
         ourShader.use();
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
@@ -197,6 +200,24 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        mixValue += 0.1f;
+        if (mixValue >= 1.0f)
+            mixValue = 1.0f;
+
+        std::cout << "mixValue:" << mixValue << std::endl;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        mixValue -= 0.1f;
+        if (mixValue <= 0.0f)
+            mixValue = 0.0f;
+        std::cout << "mixValue:" << mixValue << std::endl;
+
+    }
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
