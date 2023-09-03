5f567f9f3750da67bb0ab663a343ce02b9ec4ea3
Zheng Chen <chenzheng0430@gmail.com>
2023-09-03 03:17:20 -0400

    loaded a texture to a rectangle

--- BreakOut/src/FragmentShader.fs
+++ BreakOut/src/FragmentShader.fs
@@ -1,10 +1,12 @@
 #version 330 core
-out vec4 FragColor;
-in vec3 vertexPos;
 
+out vec4 FragColor;
 in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
 
 void main()
 {
-    FragColor = vec4(vertexPos, 1.0f);
+    FragColor = texture(texture1, TexCoord);
 }
